8d9f8f2859b322f17332b4f294adcbcb38bf8327
roshanajicherian <roshanaji777@gmail.com>
2020-06-28 15:14:13 +0530

    Complete part 3

--- WebAppUsingDjano/blog/views.py
+++ WebAppUsingDjano/blog/views.py
@@ -1,12 +1,27 @@
 from django.shortcuts import render
 from django.http import HttpResponse
-
-# home controls traffic of the homepage
+# home controls traffic of the homepage. Stored in a list of dictionaries
+posts = [
+    {'author': 'Roshan Aji Cherain',
+     'title': 'Blog post sample',
+     'date_posted': '22nd Jan 2019',
+     'content': 'lorem episulm1'
+     },
+    {'author': 'John Doe',
+     'title': 'Blog post sample by john',
+     'date_posted': '22nd Jan 2020'
+     }
+]
 
 
 def home(request):
-    return HttpResponse('<h1>Hello World</h1>')
+    context = {
+        'posts': posts
+    }
+    # Third argument of render passes the content to the html file. It must be a dictionary
+    # of the content to pass
+    return render(request, 'blog/home.html', context)
 
 
 def about(request):
-    return HttpResponse('<h1>This is the about page</h1>')
+    return render(request, 'blog/about.html', {'title': 'About'})
