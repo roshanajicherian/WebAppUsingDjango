ce07c96ed3e12e254909eb985f10eff857cc76ce
roshanajicherian <roshanaji777@gmail.com>
2020-07-01 16:22:06 +0530

    Added crispyforms

--- WebAppUsingDjano/WebAppUsingDjano/settings.py
+++ WebAppUsingDjano/WebAppUsingDjano/settings.py
@@ -39,7 +39,7 @@ INSTALLED_APPS = [
     'django.contrib.staticfiles',
     'blog.apps.BlogConfig',
     'user.apps.UserConfig',
-
+    'crispy_forms',
 ]
 
 MIDDLEWARE = [
@@ -121,3 +121,5 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/3.0/howto/static-files/
 
 STATIC_URL = '/static/'
+
+CRISPY_TEMPLATE_PACK = 'bootstrap4'
