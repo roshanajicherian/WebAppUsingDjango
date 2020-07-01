e69e6168c01f081711cdd300a1ab32a5a196477d
roshanajicherian <roshanaji777@gmail.com>
2020-07-01 12:42:48 +0530

    Completed part 6

--- WebAppUsingDjano/blog/models.py
+++ WebAppUsingDjano/blog/models.py
@@ -1,3 +1,15 @@
 from django.db import models
+from django.utils import timezone
+from django.contrib.auth.models import User
 
-# Create your models here.
+
+class Post(models.Model):
+    title = models.CharField(max_length=100)
+    content = models.TextField()
+    # do not pass timezone.now as a function even though it is
+    datePosted = models.DateTimeField(default=timezone.now)
+    # CASCADE - if a user is deleted delete the post as well
+    author = models.ForeignKey(User, on_delete=models.CASCADE)
+
+    def __str__(self):
+        return self.title
--- WebAppUsingDjano/blog/views.py
+++ WebAppUsingDjano/blog/views.py
@@ -1,22 +1,10 @@
 from django.shortcuts import render
 from django.http import HttpResponse
+from .models import Post
 # home controls traffic of the homepage. Stored in a list of dictionaries
-posts = [
-    {'author': 'Roshan Aji Cherain',
-     'title': 'Blog post sample',
-     'date_posted': '22nd Jan 2019',
-     'content': 'lorem episulm1'
-     },
-    {'author': 'John Doe',
-     'title': 'Blog post sample by john',
-     'date_posted': '22nd Jan 2020'
-     }
-]
-
-
 def home(request):
     context = {
-        'posts': posts
+        'posts': Post.objects.all()
     }
     # Third argument of render passes the content to the html file. It must be a dictionary
     # of the content to pass
