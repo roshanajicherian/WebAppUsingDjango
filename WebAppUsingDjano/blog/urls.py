dfeee15d3462be90b53778d9d811a2db33ac4a12
roshanajicherian <roshanaji777@gmail.com>
2020-07-03 15:30:34 +0530

    Complete part 10

--- WebAppUsingDjano/blog/urls.py
+++ WebAppUsingDjano/blog/urls.py
@@ -2,8 +2,14 @@ from django.urls import path, include
 from django.contrib import admin
 from . import views
 from user import views as user_views
+from .views import PostListView, PostDetailView, PostCreateView, PostUpdateView, PostDeleteView
 urlpatterns = [
 
-    path('', views.home, name='blog-home'),
-    path('about/', views.about, name='blog-about')
+    path('', PostListView.as_view(), name='blog-home'),
+    path('post/<int:pk>/', PostDetailView.as_view(), name='post-detail'),
+    path('about/', views.about, name='blog-about'),
+    path('post/new/', PostCreateView.as_view(), name='post-create'),
+    path('post/<int:pk>/update', PostUpdateView.as_view(), name='post-update'),
+    path('post/<int:pk>/delete', PostDeleteView.as_view(), name='post-delete')
+
 ]
--- WebAppUsingDjano/blog/views.py
+++ WebAppUsingDjano/blog/views.py
@@ -1,7 +1,15 @@
 from django.shortcuts import render
 from django.http import HttpResponse
 from .models import Post
+from django.contrib.auth.mixins import LoginRequiredMixin, UserPassesTestMixin
+from django.views.generic import (ListView,
+                                  DetailView,
+                                  CreateView,
+                                  UpdateView,
+                                  DeleteView)
 # home controls traffic of the homepage. Stored in a list of dictionaries
+
+
 def home(request):
     context = {
         'posts': Post.objects.all()
@@ -9,6 +17,67 @@ def home(request):
     # Third argument of render passes the content to the html file. It must be a dictionary
     # of the content to pass
     return render(request, 'blog/home.html', context)
+
+# Class Based Views
+
+
+# List View used for blogs subscription list in youtube i.e
+# anything displayed in the form of a list
+# The class inherits from the type of view
+class PostListView(ListView):
+    model = Post
+    # <app>/<model>_<view_type>.html -> default format of searching the template
+    # By specifying template name we are
+    # overriding the path where django
+    # searches for the template
+    template_name = 'blog/home.html'
+    # By default the context list is reffered
+    # as query list by django.
+    # To change that specify a varible
+    # as below with the required name
+    context_object_name = 'posts'
+    # Changes the order of display of posts. The '-' sign
+    # specified to display the posts as newest to oldest
+    ordering = ['-datePosted']
+
+
+class PostDetailView(DetailView):
+    model = Post
+
+
+class PostCreateView(LoginRequiredMixin, CreateView):
+    model = Post
+    fields = ['title', 'content']
+
+    def form_valid(self, form):
+        form.instance.author = self.request.user
+        return super().form_valid(form)
+
+
+class PostUpdateView(LoginRequiredMixin, UserPassesTestMixin, UpdateView):
+    model = Post
+    fields = ['title', 'content']
+
+    def form_valid(self, form):
+        form.instance.author = self.request.user
+        return super().form_valid(form)
+
+    def test_func(self):
+        post = self.get_object()
+        if self.request.user == post.author:
+            return True
+        return False
+
+
+class PostDeleteView(LoginRequiredMixin, UserPassesTestMixin, DeleteView):
+    model = Post
+    success_url = '/home'
+
+    def test_func(self):
+        post = self.get_object()
+        if self.request.user == post.author:
+            return True
+        return False
 
 
 def about(request):
